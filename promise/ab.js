c6869b96f9e811079faebe9194ec61a1a54dbeca
huyong <18168400272@163.com>
2018-06-14 00:27:31 +0800

    promise拿cnode数据初级版

--- promise/ab.js
+++ promise/ab.js
@@ -1,2 +1,23 @@
 //使用promise抓取cnode社区用户数据
+//引入axios
+var axios = require('axios');
+//构建调用户名的函数
+function readName() {
+    axios.get('https://cnodejs.org/api/v1/topics?page=1&tab=good&limit=1&mdrender=false')
+        .then(response => {
+            return response.data.data[0].author.loginname;
+        })
+        .then(name => { nameInfo(name); })
+        .catch(console.log);
+}
+function nameInfo(name) {
+    axios.get('https://cnodejs.org/api/v1/user/' + name)
+        .then(response => {
+            console.log(response.data.data);
+        })
+        .catch(console.log);
+}
+
+readName();
+
 
