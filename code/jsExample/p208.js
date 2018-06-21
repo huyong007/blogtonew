e7791c4e1506250edc9b9036778478bef01afe9f
huyong <18168400272@163.com>
2018-06-21 17:19:07 +0800

    html总结和js练习

--- code/jsExample/p208.js
+++ code/jsExample/p208.js
@@ -1,4 +1,4 @@
-function defineClass(
+/* function defineClass(
     constructor,
     methods,
     statics) {
@@ -25,4 +25,54 @@ var SimpleRange =
             }
         }
 
-    ),
+    ) */
+
+//数组的复制
+console.log("f1");
+
+var x = [1, 2, 3, 4, 5];
+console.log(x[1]);
+
+console.log("f2");
+var y =  [];
+console.log("f3");
+
+    console.log("f4");
+    for (var i = 0; i < x.length; i++) {
+        console.log("f5");
+         y[i] = x[i];
+    }
+    console.log("f6");
+    console.log(y[1]);
+    console.log("f7");
+    console.log(y);
+    console.log("f8");
+
+/* var a = ['a', 'b', 'c'];
+var b = [];
+for (var i = 0; i < a.length; i++) {
+    b[i] = a[i];
+}
+console.log(b);
+ */
+/* function fuzhiArrays(x) {
+    return function (y) {
+        return f(x,y);
+    }
+}
+fuzhiArrays(x)(y); */
+console.log("f9");
+
+console.log("f10");
+//数组的比较
+/* function equalArray(x,y) {
+    if(x.length!=y.length){
+        return "x,y不相等"
+    };
+    for(i=0;i<x.length;i++){
+        if(x[i]!=y[i]){
+            return "不相等"
+        }
+    };
+    return "相等"
+} */
