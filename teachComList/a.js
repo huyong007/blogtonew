421fd04a6c8ca00d91c239623b4ce323c993b1cf
huyong <18168400272@163.com>
2018-07-10 23:50:55 +0800

    js和vue的学习

--- teachComList/a.js
+++ teachComList/a.js
@@ -25,7 +25,7 @@ const myURL = new URL('http://xugaoyang.com/zone?zoneId=59d0a6c17a33600f00b862c1
 console.log(myURL.searchParams); */
 // Prints https://example.org/foo
 
-var publicKeyList = ["code1-3443", "code2-f8759", "code3-196433"];
+/* var publicKeyList = ["code1-3443", "code2-f8759", "code3-196433"];
 var lengths = [];
 (function () {
     publicKeyList.forEach(function (value) {
@@ -40,3 +40,25 @@ var lengths = publicKeyList.map(function (item) {
     return item.length;
 });
 
+ */
+/* let list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+let a = [];
+for(i=0;i<list.length;i++){
+    a[i]=list[i][0];
+}
+
+b=list.map(function (x) {
+   return  x[0];
+});
+ */
+
+let list = ['100', '99', '98'];
+
+for (let i = 0; i < list.length; i++) {
+    console.log('第' + (i+1) + "名：" + list[i]);
+}
+
+list.forEach(function (v,i) {
+    console.log('第'+(i+1)+'名:'+v);
+    
+})
